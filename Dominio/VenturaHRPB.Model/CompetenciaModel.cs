84d90d6111312c9bc8398ff7294fa3f486259498
Guilhermesav <54535484+Guilhermesav@users.noreply.github.com>
2022-04-15 16:36:55 -0300

    Add files via upload

---
+++ Dominio/VenturaHRPB.Model/CompetenciaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VenturaHRPB.Model
+{
+    public class CompetenciaModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public float Peso { get; set; }
+        
+        
+    }
+}
